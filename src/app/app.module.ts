91c102e3bd6a6acd36b2954927ec5e26ef530314
Sahib khan <sahibkhan863075@gmail.com>
2022-12-15 16:19:41 +0530

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     declarations: [
         AppComponent,
         HomeComponent,
-        NavbarComponent
     ],
     providers: [],
     bootstrap: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
+        SharedModule,
+        FontAwesomeModule
+    ],
+    exports: [
         FontAwesomeModule
     ]
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { InfocardComponent } from './infocard/infocard.component';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    InfocardComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    InfocardComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
